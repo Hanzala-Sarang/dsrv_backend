6814bb6017c630d768b1234cd2cf14b0c9f3329e
Hanzala-Sarang <Hanzalasarang01@gmail.com>
2025-04-10 23:03:59 +0530

    final

--- server.js
+++ server.js
@@ -11,11 +11,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Root route
+app.get("/", (req, res) => {
+  res.send("Welcome to the DSRV API - Server is Running ✅");
+});
+
 // Routes
 app.use("/api/contact", contactRoutes);
 
 // Server start
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
